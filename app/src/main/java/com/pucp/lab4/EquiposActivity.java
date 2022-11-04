83925a073d860245f8b16a2a17deb35d8214a65a
Leo <a20185896@pucp.edu.pe>
2022-11-04 11:34:56 -0500

    equipos

--- app/src/main/java/com/pucp/lab4/EquiposActivity.java
+++ app/src/main/java/com/pucp/lab4/EquiposActivity.java
@@ -3,12 +3,47 @@ package com.pucp.lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class EquiposActivity extends AppCompatActivity {
+
+    FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equipos);
+
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+    }
+
+    public void AgregarEquipos(View view){
+
+        HashMap<String,String> equipos = new HashMap<>();
+
+        EditText equipo1 = findViewById(R.id.editText_equipo1);
+        String equipo1Str = equipo1.getText().toString();
+
+        EditText equipo2 = findViewById(R.id.editText_equipo2);
+        String equipo2Str = equipo2.getText().toString();
+
+        equipos.put("1",equipo1Str);
+        equipos.put("2",equipo2Str);
+
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+        databaseReference.child("equipo").setValue(equipos).addOnSuccessListener(aVoid -> {
+            Log.d("msg","Equipos guardado exitosamente");
+
+        }).addOnFailureListener(e -> {
+            Log.d("msg",e.getMessage());
+        });
     }
 }
