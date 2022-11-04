60c9a217a33084688092cb800c500b85dce92c1b
Leo <a20185896@pucp.edu.pe>
2022-11-04 14:17:31 -0500

    alineacion v2

--- app/src/main/java/com/pucp/lab4/AlineacionesActivity.java
+++ app/src/main/java/com/pucp/lab4/AlineacionesActivity.java
@@ -1,14 +1,58 @@
 package com.pucp.lab4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.pucp.lab4.Adapters.AlineacionAdapter;
+import com.pucp.lab4.Adapters.HitoAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AlineacionesActivity extends AppCompatActivity {
 
+    FirebaseDatabase firebaseDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Hitos");
         setContentView(R.layout.activity_alineaciones);
+        ArrayList<Hito> listaHito = new ArrayList<>();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference ref = firebaseDatabase.getReference().child("hitos");
+        ref.get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+            @Override
+            public void onSuccess(DataSnapshot dataSnapshot) {
+                for (DataSnapshot d : dataSnapshot.getChildren()){
+                    listaHito.add(d.getValue(Hito.class));
+                }
+            }
+        });
+
+        ArrayList<String> listaJugadores = new ArrayList<>();
+        Intent intent = getIntent();
+        String comparar =(String) intent.getStringExtra("equipo");
+
+        for(Hito hito : listaHito){
+            if(hito.getEquipo().equals(comparar)){
+                listaJugadores.add(hito.getNombreJugador()+" "+hito.getApellidoJugador());
+            }
+        }
+
+        RecyclerView recyclerView =findViewById(R.id.recycleView_alineaciones);
+        AlineacionAdapter alineacionAdapter = new AlineacionAdapter(listaJugadores);
+        recyclerView.setAdapter(alineacionAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(AlineacionesActivity.this));
     }
 }
