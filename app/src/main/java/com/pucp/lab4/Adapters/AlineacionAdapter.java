d073454e309f5b3380b87d701b1613f1d716cf91
Leo <a20185896@pucp.edu.pe>
2022-11-04 13:02:48 -0500

    alineacion adapater

---
+++ app/src/main/java/com/pucp/lab4/Adapters/AlineacionAdapter.java
@@ -0,0 +1,56 @@
+package com.pucp.lab4.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pucp.lab4.Hito;
+import com.pucp.lab4.R;
+
+import java.util.ArrayList;
+
+public class AlineacionAdapter {
+    private ArrayList<String> listaJugadores;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        private final TextView textView;
+
+        public ViewHolder(View view){
+            super(view);
+            textView = (TextView) view.findViewById(R.id.textViewJugador);
+        }
+        public TextView getTextView(){
+            return getTextView();
+        }
+
+    }
+
+    public AlineacionAdapter(ArrayList<String> dataSet){
+        listaJugadores=dataSet;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.item_alineaciones,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        String mostrar = "Nombres de Jugadores: "+"\n-"+hito.getNombreJugador()+" "+hito.getApellidoJugador();
+        TextView textShow = holder.itemView.findViewById(R.id.textViewJugador);
+        textShow.setText(mostrar);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaHistorial.size();
+    }
+}
