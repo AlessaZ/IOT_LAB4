4284869075c81e3af7d9626a068470ef2b932d20
Leo <a20185896@pucp.edu.pe>
2022-11-04 11:44:20 -0500

    equipos v3

--- app/src/main/java/com/pucp/lab4/EquiposActivity.java
+++ app/src/main/java/com/pucp/lab4/EquiposActivity.java
@@ -51,5 +51,7 @@ public class EquiposActivity extends AppCompatActivity {
         });
 
         Intent intent =  new Intent(EquiposActivity.this, AdminActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
